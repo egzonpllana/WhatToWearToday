fed0d6a9b85af3e17e74b4206c84feb6ba1076aa
Egzon <docpllana@gmail.com>
2020-03-20 15:11:58 +0100

    Initial commit.

--- README.md
+++
@@ -1,2 +0,0 @@
-# WhatToWearToday
-Simple app that use current user location to get weather data from public api.
---
+++ WhatToWearToday/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WhatToWearToday
+//
+//  Created by Egzon Pllana on 3/20/20.
+//  Copyright © 2020 Native Coders. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+
+    }
+}
+
