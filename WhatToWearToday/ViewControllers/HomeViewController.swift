a3b98a9d09499a9cc862ccf82f0da95e6bc31ef7
Egzon <docpllana@gmail.com>
2020-03-20 16:13:15 +0100

    Main -> Home -> Design of the screen.
    Added new GUI extensions:
    - UIExtendedView
    - UIRoundedView.

--- WhatToWearToday/ViewControllers/HomeViewController.swift
+++ WhatToWearToday/ViewControllers/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  WhatToWearToday
 //
 //  Created by Egzon Pllana on 3/20/20.
@@ -8,12 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
+
+    // MARK: - View life cycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
 
+        // Do any additional setup after loading the view.
     }
 }
-
